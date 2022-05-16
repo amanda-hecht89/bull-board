ce07196248d9658e9f20ac93b1c961dac793d5b7
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-16 17:50:04 -0600

    add render function

---
+++ utils.js
@@ -0,0 +1,11 @@
+export function renderState(state) {
+    const div = document.createElement('div');
+    div.classList.add('state');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = state.name;
+
+    const p = document.createElement('p');
+    p.textContent = `At ~${state.elevation}, located in ${state.location}. Wth ${state.atttractions}, there is so much to do! Price is $${state.price}+.`;
+
+}
